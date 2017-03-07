aa073485de07b73429904bdc31959722824edfa9
charles-r-earp <charles.r.earp@gmail.com>
2017-03-07 15:56:49 -0800

    Update Search.hpp

--- include/Search.hpp
+++ include/Search.hpp
@@ -42,6 +42,41 @@ namespace cvt {
         }
     }
     
+    template <typename Graph> void a_star_action_search(Graph graph, typename Graph::Vertex start, typename Graph::Vertex goal) {
+    
+        using Vertex = typename Graph::Vertex;
+        using Edge = typename Graph::Edge;
+        
+        std::priority_queue<Edges> frontier;
+        frontier.push(start);
+        
+        //std::unordered_set<Vertex> visited;
+        //visited.insert(start);
+        
+        std::map<Vertex, Edge> visited_edges;
+        
+        while(!frontier.empty()) {
+            auto current = frontier.front();
+            frontier.pop();
+            
+            std::cout << "Visiting " << current << std::endl;
+            
+            if (current == goal) {
+                break;
+            }
+            
+            for (auto next_edge : graph.neighbors(current)) {
+                
+                Vertex next = next_edge; 
+                
+                if (!visited_edges.count(next)) {
+                    frontier.push(next_edge);
+                    visited_edges.insert(next_edge);
+                }
+            }
+        }
+    }
+    
 }
 
 
